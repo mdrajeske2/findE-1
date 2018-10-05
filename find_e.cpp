d3061107006e4956126a57824f47ccd5a7822804
mdrajeske2 <43419272+mdrajeske2@users.noreply.github.com>
2018-10-04 21:37:58 -0500

    Add files via upload

--- find_e.cpp
+++ find_e.cpp
@@ -2,22 +2,27 @@
 #include "stdafx.h"
 #include <cmath>
 
+double e = 1;
+double tol;
+
 void find_e() {
 	
 	//variables. Difference for evaluating tolerance, denominator for Euler formula, e and xn1 for
 	//calculating difference
-	e = 1;
-	double difference;
-	double en1 = 1;
-	double denominator = 1;
+	e = 1; //euler's constant
+	double difference; 
+	double nextPos = 1; //n + 1
+	double denominator = 1; //what 1 is being divided by
+	double n = 0; //
 
 	//calculates Euler's number to user's tolerance
 	do {
-		e = e / denominator;
-		denominator *= denominator + 1; 
-		en1 = e / denominator;
-		difference = abs(e - en1);
-		e = en1;
-	} while (difference < ::tol);
+		n++;
+		denominator *= n;
+		nextPos = e / denominator; 
+		 
+		difference = abs(e - nextPos);
+		e = nextPos;
+	} while (difference < tol);
 	
 }
